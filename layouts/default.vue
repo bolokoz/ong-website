d3db0f0d0f5f1d5195f5e1859d2e1309d3f0e476
Yuri Furusho Becker <yurifbeck@gmail.com>
2018-03-29 23:28:02 -0400

    mudei footer mas sei la

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,60 +37,20 @@
       </v-container>
     </v-content>
 
-  <v-footer height="auto">
-    <v-card flat tile>
-      <v-card-title class="teal white--text">
-        <strong class="subheading">Get connected with us on social networks!</strong>
-        <v-spacer></v-spacer>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          icon
-          dark
-          class="mx-3"
-        >
-          <v-icon size="24px">{{ icon }}</v-icon>
-        </v-btn>
-      </v-card-title>
-      <v-card-text class="grey lighten-3">
-        <v-layout>
-          <v-flex
-            v-for="(col, i) in rows"
-            :key="i"
-            xs3
-          >
-            <span class="body-2" v-text="col.title.toUpperCase()"/>
-            <div
-              v-for="(child, i) in col.children"
-              :key="i"
-              v-text="child"
-            />
-          </v-flex>
-          <v-flex xs3 layout column>
-            <span class="body-2">CONTACT</span>
-            <div>
-              <v-icon size="18px" class="mr-3">fas fa-home</v-icon>
-              New York, NY 10012, US
-            </div>
-            <div>
-              <v-icon size="18px" class="mr-3">fas fa-envelope</v-icon>
-              info@example.com
-            </div>
-            <div>
-              <v-icon size="18px" class="mr-3">fas fa-phone</v-icon>
-              + 01 234 567 88
-            </div>
-            <div>
-              <v-icon size="18px" class="mr-3">fas fa-print</v-icon>
-              + 01 234 567 89
-            </div>
-          </v-flex>
-        </v-layout>
-      </v-card-text>
-      <v-card-actions class="grey lighten-2 justify-center">
+  <v-footer height="auto" class="grey darken-3">
+    <v-layout row wrap justify-center>
+      <v-btn
+        color="white"
+        flat
+        v-for="link in links"
+        :key="link"
+      >
+        {{ link }}
+      </v-btn>
+      <v-flex xs12 py-3 text-xs-center white--text>
         &copy;2018 — <strong>Vuetify</strong>
-      </v-card-actions>
-    </v-card>
+      </v-flex>
+    </v-layout>
   </v-footer>
   </v-app>
 </template>
@@ -100,20 +60,7 @@
     data () {
       return {
         drawer: false,
-        rows: [
-          {
-            title: 'Company Name',
-            children: ['Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit']
-          },
-          {
-            title: 'Products',
-            children: ['MDBootstrap', 'MDWordPress', 'BrandFlow', 'Bootstrap Angular']
-          },
-          {
-            title: 'Useful Links',
-            children: ['Your account', 'Become an Affiliate', 'Shipping Rates', 'Helper']
-          }
-        ],
+        links: ['Home', 'About Us', 'Team', 'Services', 'Blog', 'Contact Us'],
         icons: ['fab fa-facebook', 'fab fa-twitter', 'fab fa-google-plus', 'fab fa-linkedin', 'fab fa-instagram'],
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
