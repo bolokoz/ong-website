ab81f2bbb6c9853444f6733c5386aac0afd1bff5
Yuri Furusho Becker <yurifbeck@gmail.com>
2018-04-01 11:09:30 -0400

    arrumei um pouco footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,8 +23,8 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar 
-      scroll-off-screen 
-      color="transparent"
+      color="white"
+      dense
       app>
       <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title v-text="title"></v-toolbar-title>
@@ -42,13 +42,14 @@
       <v-btn
         color="white"
         flat
-        v-for="link in links"
-        :key="link"
+        v-for="item in footer"
+        :key="item"
+        :to="item.link"
       >
-        {{ link }}
+        {{ item.title }}
       </v-btn>
-      <v-flex xs12 py-3 text-xs-center white--text>
-        &copy;2018 — <strong>Vuetify</strong>
+      <v-flex xs12 py-1 text-xs-center white--text>
+        &copy;2018 — <strong>asdasd</strong>
       </v-flex>
     </v-layout>
   </v-footer>
@@ -60,14 +61,22 @@
     data () {
       return {
         drawer: false,
-        links: ['Home', 'About Us', 'Team', 'Services', 'Blog', 'Contact Us'],
+        footer: [
+          { title: 'Home', link: '/' },
+          { title: 'Sobre nos', link: '/sobre' },
+          { title: 'Participe', link: '/participe' },
+          { title: 'Agenda', link: '/agenda' },
+          { title: 'Blog', link: '/blog' },
+          { title: 'Doacoes', link: '/doar' },
+          { title: 'Contanto', link: '/contato' }
+        ],
         icons: ['fab fa-facebook', 'fab fa-twitter', 'fab fa-google-plus', 'fab fa-linkedin', 'fab fa-instagram'],
         items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Blog', to: '/blog' },
+          { icon: 'apps', title: 'Home', to: '/' },
+          { icon: 'bubble_chart', title: 'Sobre nos', to: '/sobre' },
           { icon: 'bubble_chart', title: 'Participe', to: '/participe' },
           { icon: 'bubble_chart', title: 'Agenda', to: '/agenda' },
-          { icon: 'add', title: 'Fazer doação', to: '/doar' },
+          { icon: 'add', title: 'Blog', to: '/blog' },
           { icon: 'bubble_chart', title: 'Sobre o projeto', to: '/sobre' }
         ],
         title: 'Nome Exemplo ONG'
